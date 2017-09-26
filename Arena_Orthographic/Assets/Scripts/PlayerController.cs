6391d0f8544b0c25e00dc30d90b9d7fc0a0c3aca
Dennis-HomePC <dennis.slavinsky@gmail.com>
2017-09-26 01:09:59 -0400

    Added a preset for the orthographic game view
    The player's Z wil be locked. Working on the Networked orthographic view with Unity Networking

---
+++ Arena_Orthographic/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+
+    [SerializeField]
+    private float movement_Speed = 5f;
+
+    private PlayerMotor motor;
+
+	void Start () {
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float _xMovement = Input.GetAxisRaw("Horizontal");
+        float _Jump_Z = Input.GetAxisRaw("Vertical");//remember to use this input for jumping up 
+        //or moving inside the map
+
+        Vector3 _moveHorizontal = transform.right * _xMovement;
+        Vector3 _moveVertical = transform.forward * _Jump_Z;
+
+        Vector3 velocity = _moveHorizontal.normalized * movement_Speed;//make sure to see for how long is the 
+        //key pressed as the velocity and acceleration should be applied according to the button presses
+
+        motor.SendVelocity(velocity);
+	}
+}
---
+++ Arena_Orthographic/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+    public void SendVelocity(Vector3 _Velocity)
+    {
+        velocity = _Velocity;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+}
